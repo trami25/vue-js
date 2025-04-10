4f6e9775194fd1bd1d1f9bad8ae336c33496b306
Nikola Mitrović <105147473+trami25@users.noreply.github.com>
2025-04-10 14:34:20 +0200

    first version

---
+++ primary/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+      <h1>URL Shortener</h1>
+      <input v-model="originalUrl" type="text" placeholder="Enter a URL... "/>
+      <button @click="shortenUrl">Shorten</button>
+
+      <div v-if="shortenedUrl" class="result">
+        <p>Shortened URL: </p>
+        <a :href="shortenedUrl" target="_blank"> {{ shortenedUrl }}</a>
+      </div>
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      originalUrl: "",
+      shortenedUrl: "",
+    };
+  },
+
+  methods:{
+    shortenUrl() {
+      // Simulate URL shortening
+      if (this,originalUrl.trim() === "") return;
+
+      const randomId = Math.random().toString(36).substring(2, 8);
+      this.shortenUrl = `https://short.ly/${randomId}`
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container{
+  max-width: 400px;
+  margin: 50px auto;
+  padding: 20px;
+  text-align: center;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+}
+
+input{
+  width: 80%;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+button{
+  padding: 10px 20px;
+}
+
+.result{
+  margin-top: 20px;
+}
+
+
+</style>
---
+++ primary/src/components/App.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="container">
+        <h1>URL Shortener</h1>
+    </div>
+</template>
